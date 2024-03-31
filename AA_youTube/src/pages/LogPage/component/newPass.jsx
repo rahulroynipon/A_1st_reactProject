fecedd42af9924a2f8501f9badb33d10ba55e3c7
rahulroynipon <rahulroynipon@gmail.com>
2024-04-01 05:26:52 +0600

    add

--- AA_youTube/src/pages/LogPage/component/newPass.jsx
+++ AA_youTube/src/pages/LogPage/component/newPass.jsx
@@ -1,8 +1,30 @@
+import passIcon from "/src/assets/password.svg";
 import PopUp from "../../../component/popUp";
+import { useCallback, useEffect, useState } from "react";
 
 function NewPass({ newPassPage, setpassPage }) {
   const theme = "#7150B7";
   const btn = `px-4 border border-gray-300 rounded outline-none focus:ring-1 focus:ring-[#7150B7]`;
+  const [newPass, setPass] = useState("");
+  const [rePass, setRePass] = useState("");
+  const [typeOTP, setOTP] = useState("");
+  const [generatedOTP, setGeneratedOTP] = useState("");
+
+  const OtpGen = useCallback(() => {
+    const charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=";
+    let OTP = "";
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      OTP += charset[randomIndex];
+    }
+    return OTP;
+  }, []);
+
+  useEffect(() => {
+    const otp = OtpGen();
+    setGeneratedOTP(otp);
+  }, [newPassPage, OtpGen]);
 
   return (
     <>
@@ -11,8 +33,9 @@ function NewPass({ newPassPage, setpassPage }) {
         Title={"New password"}
         Click={newPassPage}
         setClick={setpassPage}
-        misData={true}
-        note={`the OTP code is ${1}`}
+        misData={newPassPage}
+        error={passIcon}
+        note={`the OTP code is ${generatedOTP}`}
         color={"bg-green-400"}
         Section={
           <form
@@ -20,6 +43,8 @@ function NewPass({ newPassPage, setpassPage }) {
             className="flex flex-col items-center px-12 mt-5"
           >
             <input
+              onChange={(e) => setPass(e.target.value)}
+              value={newPass}
               className={`${btn} py-2 w-full bg-gray-100`}
               type="password"
               placeholder="new password"
@@ -27,6 +52,8 @@ function NewPass({ newPassPage, setpassPage }) {
             />
 
             <input
+              onChange={(e) => setRePass(e.target.value)}
+              value={rePass}
               className={`${btn} py-2 w-full my-3 bg-gray-100`}
               type="password"
               placeholder="retype new password"
@@ -34,6 +61,8 @@ function NewPass({ newPassPage, setpassPage }) {
             />
 
             <input
+              onChange={(e) => setOTP(e.target.value)}
+              value={typeOTP}
               className={`${btn} py-2 w-full bg-gray-100`}
               type="text"
               placeholder="Enter the otp code"
@@ -41,6 +70,7 @@ function NewPass({ newPassPage, setpassPage }) {
             />
 
             <button
+              onClick={() => setpassPage(false)}
               className={`${btn} mt-3 py-2 w-24 bg-[${theme}] hover:bg-[#684ba7] text-white font-semibold`}
             >
               Save
